dbcc55dd3fe837aca0ecf826a02d9b2b543ad1e2
Saga Kortesaari <sagak@student.chalmers.se>
2020-11-19 22:35:02 +0100

    added add method for images and search method to fetch random image

--- api.py
+++ api.py
@@ -1,23 +1,25 @@
 import flask
 from flask import request, jsonify
 from connect import collection
+import random
 
 app = flask.Flask(__name__)
 
-@app.route('/api/add/<word>')
-def add(word):
-    if (collection.find_one({"word" : word}) == None):
-        obj = {
-            "word": word
-        }
-        collection.insert_one(obj)
-        return jsonify(word, "successfully added!")
+@app.route('/add', methods=["POST"])
+def add():
+    query = request.get_json()
+    insert = request.get_json()
+    collection.insert_one(query)
+    return jsonify("successfully inserted!")
+
+@app.route('/search/<type>')
+def search(type):
+    if (collection.find_one({"type": type}) != None):
+        results = list(collection.find({"type": type}))
+        print(results)
+        return "<img src=" + random.choice(results)["url"] + ">"
     else:
         flask.abort(404)
 
-@app.route('/api/search/<word>')
-def search(word):
-    if (collection.find_one({"word": word}) != None):
-        return jsonify(word, "found!")
-    else:
-        flask.abort(404)
+
+
